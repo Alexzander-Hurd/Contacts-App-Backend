d57dc8f8f2a599413aeb2c974c1634cb65c26074
Alexzander Hurd <ajghurd@gmail.com>
2025-09-23 08:00:09 +0100

    Add key attribute  and ID field to refresh tokens

---
+++ ContactsApp/Migrations/20250918132622_added key to refresh model.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContactsApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedkeytorefreshmodel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_refresh_tokens",
+                table: "refresh_tokens");
+
+            migrationBuilder.AddColumn<string>(
+                name: "id",
+                table: "refresh_tokens",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_refresh_tokens",
+                table: "refresh_tokens",
+                column: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_refresh_tokens",
+                table: "refresh_tokens");
+
+            migrationBuilder.DropColumn(
+                name: "id",
+                table: "refresh_tokens");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_refresh_tokens",
+                table: "refresh_tokens",
+                column: "token");
+        }
+    }
+}
